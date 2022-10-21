737e0650d643f189964d6429b7d53e98b2c9bb7c
bruh-git <brunac.cardososilva@gmail.com>
2022-10-21 01:12:35 -0300

    feat: req 11

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,8 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    translation = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    for job in translation:
+        assert job.get("titulo") is None
+        assert job.get("title") is not None
